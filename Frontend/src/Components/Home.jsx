ad7dc033b6d38baf9a619b3c3468901b0000d1d7
Endev0ur <devlpr.shubham@gmail.com>
2025-04-16 10:39:50 +0530

    Day 3 : Frontend (Add Blur Effect On clicking different section);

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,35 +1,69 @@
-import React from 'react'
-import Mainbar from './Mainbar';
-
+import React, { useRef, useState } from "react";
+import Mainbar from "./Mainbar";
 
 const Home = () => {
+  const [openForm, setOpenForm] = useState(false);
+  const summarizerref = useRef(false);
+  const atsCheckref = useRef(false);
+  const generateResumeref = useRef(false);
+
   return (
-    <div className='h-screen w-full flex justify-center items-center flex-col bg-gray-300'>
-      <div className="h-[70%] w-[100%] p-5 sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] flex justify-start items-start flex-col bg-gray-300 pt-10 xl:pt-20 overflow-y-scroll no-scrollbar">
-        <h1 className='text-2xl font-bold underline sm:text-3xl xl:text-4xl'>How to Use the Product ? </h1>
-        <br />
-        <br className='hidden sm:block'/>
-        <p className='text-xl font-bold sm:text-2xl xl:text-3xl'>Feature 1 : Summarizer</p>
-        <br className='hidden sm:block'/>
-        <p className='pl-6 text-lg font-bold sm:text-xl xl:text-2xl text-gray-700'>I. Click & then copy paste the job description and extract the keywords for you resume</p>
-        <br />
-        <br className='hidden sm:block'/>
-        <p className='text-xl font-bold sm:text-2xl xl:text-3xl'>Feature 2 : ATS Score</p>
-        <br className='hidden sm:block'/>
-        <p className='pl-6 text-lg font-bold sm:text-xl xl:text-2xl text-gray-700'>I. Upload Your resume and check the ATS Score Based on Job Description</p>
-        <br />
-        <br className='hidden sm:block'/>
-        <p className='text-xl font-bold sm:text-2xl xl:text-3xl'>Feature 3 : Generate Resume</p>
-        <br className='hidden sm:block'/>
-        <p className='pl-6 text-lg font-bold sm:text-xl xl:text-2xl text-gray-700'>I. Generate the ATS Friendly resume based on job description <br /> (* different resume for different job description with one click)</p>
-
+    <div className="relative">
+      <div className="h-screen w-full flex justify-center items-center flex-col bg-gray-300">
+        <div className="h-[70%] w-[100%] p-5 sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] flex justify-start items-start flex-col bg-gray-300 pt-10 xl:pt-20 overflow-y-scroll no-scrollbar">
+          <h1 className="text-2xl font-bold underline sm:text-3xl xl:text-4xl">
+            How to Use the Product ?{" "}
+          </h1>
+          <br />
+          <br className="hidden sm:block" />
+          <p className="text-xl font-bold sm:text-2xl xl:text-3xl">
+            Feature 1 : Summarizer
+          </p>
+          <br className="hidden sm:block" />
+          <p className="pl-6 text-lg font-bold sm:text-xl xl:text-2xl text-gray-700">
+            I. Click & then copy paste the job description and extract the
+            keywords for you resume
+          </p>
+          <br />
+          <br className="hidden sm:block" />
+          <p className="text-xl font-bold sm:text-2xl xl:text-3xl">
+            Feature 2 : ATS Score
+          </p>
+          <br className="hidden sm:block" />
+          <p className="pl-6 text-lg font-bold sm:text-xl xl:text-2xl text-gray-700">
+            I. Upload Your resume and check the ATS Score Based on Job
+            Description
+          </p>
+          <br />
+          <br className="hidden sm:block" />
+          <p className="text-xl font-bold sm:text-2xl xl:text-3xl">
+            Feature 3 : Generate Resume
+          </p>
+          <br className="hidden sm:block" />
+          <p className="pl-6 text-lg font-bold sm:text-xl xl:text-2xl text-gray-700">
+            I. Generate the ATS Friendly resume based on job description <br />{" "}
+            (* different resume for different job description with one click)
+          </p>
+        </div>
+        <div className="h-[30%] w-[100%] flex items-center justify-center">
+          <Mainbar
+            openForm={openForm}
+            setOpenForm={setOpenForm}
+            summarizerref={summarizerref}
+            generateResumeref={generateResumeref}
+            atsCheckref={atsCheckref}
+          ></Mainbar>
+        </div>
       </div>
-      <div className='h-[30%] w-[100%] flex items-center justify-center'>
-        <Mainbar></Mainbar>
-      </div>
+      {openForm && (
+        <div className="fixed inset-0 z-50 backdrop-blur-md bg-black/30">
+          {summarizerref.current && <div>summarizer</div>}
+          {atsCheckref.current && <div>atscheck</div>}
+          {generateResumeref.current && <div>generateresume</div>}
+        </div>
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
