6f37043f8c52b0f0e82f6ad132a39ae2d99ef7b5
Endev0ur <devlpr.shubham@gmail.com>
2025-04-25 16:54:00 +0530

    Day 10 : Backend (protect route created) (*done)

---
+++ Backend/Middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+
+
+
+/* a function is created here which is a validation function which checks whether the user is loggedin or not */
+
+const protect = ( req , res ,next )=> {
+
+  // step1 : get the token from cookies
+  const token = req.cookies.token;
+  /* check for if the token is present or not */
+  if(!token){
+    return res.status(401).json({
+      success : false,
+      message : "No token is there!"
+    })
+  }
+
+  /* this is for if token is found */
+
+  try{
+
+    // let's verify the token
+    const user = jwt.verify(token , process.env.SECRET_TOKEN);
+    req.user = user;
+
+    next();
+
+  }
+  catch(error){
+    return res.status(401).json({
+      success : false,
+      message : "Invalid or expired Token",
+    })
+  }
+
+}
