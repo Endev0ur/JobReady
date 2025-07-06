e430f879dd524d9627f62f6e5c092c18cc7bc0ed
Endev0ur <devlpr.shubham@gmail.com>
2025-07-06 20:44:00 +0530

    Do some changes

--- Frontend/src/Components/Profile.jsx
+++ Frontend/src/Components/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = () => {
 
   return (
      <>
-      <div className="min-h-screen xl:h-screen w-full flex flex-col xl:flex-row justify-around items-center bg-gradient-to-br from-[#0f172a] to-[#1e293b] p-2 md:p-3 lg:p-4 xl:p-7">
+      <div className="h-[100%] xl:h-screen w-full flex flex-col xl:flex-row justify-around items-center bg-gradient-to-br from-[#0f172a] to-[#1e293b] p-2 md:p-3 lg:p-4 xl:p-7">
         <ProfileLeftBlock />
         <ProfileRightBlock />
       </div>
